872b67b8c5383f2960d5bc491ee08df369c0a187
Olawale Hameed <walham203378@gmail.com>
2020-12-28 02:31:10 +0100

    created the input receiver component

---
+++ src/components/input.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Enter a GitHub Username</h2>
+        <form id="form_control">
+            <input type="text" name="" id="input">
+            <input type="submit" value="Check">
+        </form>
+    </div>
+</template>
+<script>
+export default {
+    name: "Input"
+}
+</script>
+<style>
+
+</style>
+    
